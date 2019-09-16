6c6a91f955f2ef6b04b55063e2310ab53d438a2c
20rohrichtt <taylorrohrich@gmail.com>
2019-09-16 12:22:53 -0400

    week 1 inclass

---
+++ inclass/week1/programming_basics.js
@@ -0,0 +1,63 @@
+// var myString = "hello";
+// var myNumber = 12;
+// console.log(myNumber);
+// myNumber = 13;
+// console.log(myNumber);
+// let myString = "hello";
+// let myNumber = 12;
+// console.log(myNumber);
+// myNumber = 13;
+// console.log(myNumber);
+
+// var myVar = "test";
+// {
+//   var myVar = "hello";
+// }
+// console.log(myVar);
+
+// let myLet = "test";
+// {
+//   let myLet = "hello";
+// }
+// console.log(myLet);
+
+// const myConst = "cantChange";
+// console.log(myConst);
+// myConst = "error";
+
+// console.log(myConst);
+
+// 5 >= 5 ? console.log("true!") : console.log("false!");
+
+// 5 > 5 && console.log("true!");
+
+// let course = "pioneer";
+
+// switch (course) {
+//   case "source":
+//     console.log(":(");
+//     break;
+//   case "mobile":
+//     console.log(":)");
+//     break;
+//   default:
+//     console.log("no course provided.");
+// }
+
+// function myFunction(name, course) {
+//   return `Hello ${name}, from ${course}!`;
+// }
+
+// let res = myFunction("Taylor", "Mobile");
+// console.log(res);
+
+let myObject = {
+  name: "Taylor",
+  age: 21,
+  school: {
+    name: "UVA",
+    year: 4
+  }
+};
+
+console.log(myObject.school.name);
