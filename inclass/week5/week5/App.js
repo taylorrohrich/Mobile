e35c495618a85a914f9daa1defbabc765f1cffa0
20rohrichtt <taylorrohrich@gmail.com>
2019-10-20 14:41:35 -0400

    week 5:

--- inclass/week5/week5/App.js
+++ inclass/week5/week5/App.js
@@ -1,20 +1,33 @@
 import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Image
-        style={{ height: 250, width: 250 }}
-        source={{
-          uri:
-            "https://www.thecocktaildb.com/images/media/drink/athdk71504886286.jpg"
-        }}
-      />
-    </View>
-  );
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Image
+//           style={{ height: 250, width: 250 }}
+//           source={{
+//             uri:
+//               "https://www.thecocktaildb.com/images/media/drink/athdk71504886286.jpg"
+//           }}
+//         />
+//       </View>
+//     );
+//   }
+// }
+
+export default class App extends React.Component {
+  renderMyArray = array => {
+    return array.map(arrString => {
+      return <Text>{arrString}</Text>;
+    });
+  };
+  render() {
+    const myArray = ["hello", "my", "name", "is", "taylor"];
+    return <View style={styles.container}>{this.renderMyArray(myArray)}</View>;
+  }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
